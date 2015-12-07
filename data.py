d2b1dbf1286e87e81d08cf37f49d8b38578ccfe9
Joshua van Kleef <joshua.van.kleef@gmail.com>
2015-12-07 18:19:14 +0100

    Implemented Train and Data Classes. Still need to be implemented Data.PrepareTrainTestSet

--- data.py
+++ data.py
@@ -1,33 +1,69 @@
-class data
-	
+import re
+import random
 
+class Data:
+
+	@staticmethod
 	def LoadFile(filename):
 		#Pakt het tekstbestand en zet deze om naar een geschikte dataset
 		#Het formaat waarin de documenten gespecificeerd worden in het tekstbestand is als volgt: {documentnaam} {klassenaam}
 		#De inhoud van een tekstbestand kan er als volgt uitzien bij bijvoorbeeld een blog:
 		#blogs/F/F-test1.txt F
+		dataset = {}
+		with open(filename) as f:
+			for line in f:
+				document = re.split(' ',line)
+				documentFile = document[0]
+				documentclass = document[1].strip()
+				with open(documentFile) as d:					
+					lines = d.read()
+					bagOfWords = re.split(' ',lines)
+					normalizedBagOfWords = Data.Normalize(bagOfWords)
+					#Als de klasse not niet bestaat, maak hem aan
+					if not documentclass in dataset.keys():
+						dataset[documentclass] = {}
+				    #Het document komt genormaliseerd in de dataset te staan
+					dataset[documentclass][documentFile] = normalizedBagOfWords
 		return dataset
 
+
+	#### MOET NOG GEIMPLEMENTEERD WORDEN ####
+	@staticmethod
 	def PrepareTrainTestSet(dataset, ratio):
 		#Pakt de dataset en split deze op in traindata en testdata naar de ratio die gespecificeerd is.
-		normalize(dataset)
+
+
 		return [trainingSet, testSet]
 
-	def ExtractClasses(dataset):
-		#Herleid alle mogelijke klasses die in de dataset voorkomen.
-		return classes
 
-
-	def Normalize(dataset):
+	@staticmethod
+	def Normalize(document):
 		#Haalt alle interpunctie en hoofdletters weg
 		#Voorbeeld: Hello, everyone. Do you like the new layout?
 		#wordt: [hello, everyone, do, you, like, the, new, layout]
-		return normalizedDataset
+		#Zet alle woorden om naar kleine letters
+		normalizedDocument = [d.lower() for d in document]
+		#Haal alle \n, \r's weg
+		normalizedDocument = [d.strip() for d in normalizedDocument]		
+		#Haal alle interpunctie weg
+		normalizedDocument = [re.sub(r"[^a-z]", "", d) for d in normalizedDocument]
+		#Haal alle items weg die leeg zijn
+		normalizedDocument = filter(None, normalizedDocument)
+		return normalizedDocument
 
+	@staticmethod
 	def GetDocument(dataset):
 		#Pakt een willekeurige document uit de dataset.
 		return document
+def main():
+		 dataset =  Data.LoadFile('dataset.txt')
+		 print "hello"
+		 print dataset
+	
 
+
+
+main()
 
 
 
--- train.py
+++ train.py
@@ -1,27 +1,73 @@
-class train
+from data import Data
 
+class Train:
+	@staticmethod
 	def ExtractVocabulary(dataset):
 		#Herleid de volledige vocubulary van alle woorden die voorkomen in de dataset
+
+		vocubulary = []
+		for c in dataset:
+			for d in dataset[c]:
+				vocubulary.extend(dataset[c][d])
+		vocubulary = set(vocubulary)
 		return vocubulary
 
+	@staticmethod
 	def TrainMultinomialNaiveBayes(classes, dataset):
 		# De specificatie van Probabilistic Inference and Bayesian Classification (Blz. 19) van Rieks op den Akker
 		# moet hier geimplementeerd worden
+
+		vocubulary = ExtractVocabulary(dataset)
+		numberofDocs = CountNumberOfDocs(dataset)
+		prior = {}
+		condprob = {}
+		for c in classes:
+			nDocsInClass = CountDocsInClass(dataset, c)
+			prior[c] = nDocsInClass/numberofDocs
+			concText = ConcatenateAllTextsOfDocsInClass(dataset, c)
+			for t in vocubulary:
+				nTokens = CountTokensOfTerm(concText, t)
+				for t2 in vocubulary:
+					nTokens2 += CountTokensOfTerm(conctext, t2)
+				condprob[t][c] = (nTokens + 1)/nTokens2 
+
 		return [vocubulary, prior, condprob]
 
+	@staticmethod
+	def ExtractClasses(dataset):
+		#Herleid alle mogelijke klasses die in de dataset voorkomen.
+		return dataset.keys()
 
+	
+	@staticmethod
 	def CountNumberOfDocs(dataset):
-		# Telt het aantal documenten in de gehele dataset
+	# Telt het aantal documenten in de gehele dataset
+		N = 0
+		for C in dataset:
+			N += Train.CountDocsInClass(dataset, C)			
 		return N
 
-	def CountDocsInClass(dataset, class):
+	@staticmethod
+	def CountDocsInClass(dataset, c):
 		# Telt het aantal documenten in een klasse
-		return NinClass
+		return  len(dataset[c])
 
+	@staticmethod
 	def CountTokensOfTerm(concatenatedText, term):
 		# Telt hoe vaak een woord voorkomt in de samengevoegde tekst.
-		return tct
+		return concatenatedText.count(term)
 
-	def ConcatenateAllTextsOfDocsInClass(dataset, class):
+	@staticmethod
+	def ConcatenateAllTextsOfDocsInClass(dataset, c):
 		# Voegt alle tekst van alle documenten in een klasse samen
-		return  concatenatedText
+		concatenatedText = []
+		for d in dataset[c]:
+			concatenatedText.extend(dataset[c][d])
+		return concatenatedText
+def main():
+	dataset =  Data.LoadFile('dataset.txt')
+	concatenatedText = Train.ConcatenateAllTextsOfDocsInClass(dataset, 'M')
+	print Train.ExtractVocabulary(dataset)
+
+
+main()
