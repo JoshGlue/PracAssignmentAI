1472c43a0c0f3e19c8842b8c35c057d418395761
Tjeerd Jan <tj_heeringa@hotmail.com>
2015-12-07 22:42:13 +0100

    Classify uitgewerkt.

--- classify.py
+++ classify.py
@@ -1,11 +1,24 @@
-class classifier
+import math
+import numpy
+class classifier:
+        @staticmethod
+        def ApplyMultinomialNaiveBayes(classes ,vocubulary, prior, condprob, document):
+        # De specificatie van Probabilistic Inference and Bayesian Classification (Blz. 20) van Rieks op den Akker
+        # moet hier geimplementeerd worden
+                W = ExtractTokensFromDoc(vocubulary, document)
+                for c in classes:
+                        score[c] = math.log(prior[c])
+                        for t in W:
+                                score[c] += math.log(condprob[t][c])
+                topClass = numpy.argmax(score)
+                return [topClass,score[topClass]]
 
-	
-	def ApplyMultinomialNaiveBayes(classes ,vocubulary, prior, condprob, document):
-	  	# De specificatie van Probabilistic Inference and Bayesian Classification (Blz. 20) van Rieks op den Akker
-		# moet hier geimplementeerd worden
-		return [topClass, score] 
+        @staticmethod
+        def ExtractTokensFromDoc(vocabulary, document):
+                # Geeft alle woorden terug die in het documenet voorkomt
+                W = [word for word in document if word in vocubulary]                
+                return set(W)
 
-	def ExtractTokensFromDoc(vocubulary, document):
-		# Geeft alle woorden terug die in het documenet voorkomt
-		return W
+#def main():
+#        print classifier.ExtractTokensFromDoc()
+#main()
